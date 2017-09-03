e5e27180efee2498c9968ecb2ba494f5eae7957a
Catalin <catapara@gmail.com>
2017-09-03 11:31:09 +0300

    Azure dev deploy trials.

--- app.js
+++ app.js
@@ -47,36 +47,36 @@ if(process.env.NODE_ENV){
     app.use(morgan('dev'));
 }
 
-//Body parser middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-//TODO store secret as an environmental variable
-app.use(expressSession({
-    secret:'hj9g9897532u8904fsuig34534gggd',
-    resave: false,
-    saveUninitialized: false,
-    store: new MongoStore({mongooseConnection: mongoose.connection})
-}));
+////Body parser middleware
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended: false}));
+//app.use(cookieParser());
+////TODO store secret as an environmental variable
+//app.use(expressSession({
+//    secret:'hj9g9897532u8904fsuig34534gggd',
+//    resave: false,
+//    saveUninitialized: false,
+//    store: new MongoStore({mongooseConnection: mongoose.connection})
+//}));
 
 //Setting the static directory where the client files(css, js) are kept
 app.use(express.static(path.join(__dirname, 'client')));
-app.use(passport.initialize());//Grabs the data from the session
-app.use(passport.session());//Puts data into local session
+//app.use(passport.initialize());//Grabs the data from the session
+//app.use(passport.session());//Puts data into local session
 
 // This middleware is used for validating fields, the error formatter is used to format the error message which will be
 // send to the client.
-app.use(expressValidator({
-    errorFormatter: validator.errorFormatter,
-    customValidators: validator.customValidators
-}));
+//app.use(expressValidator({
+//    errorFormatter: validator.errorFormatter,
+//    customValidators: validator.customValidators
+//}));
 
-// We set the routes the data will take
-app.use("/", routes);
-app.use("/user", usersRoute);
-app.use("/dojos", dojosRoute);
-app.use("/events", eventsRoute);
-app.use("/badges", badgesRoute);
+//// We set the routes the data will take
+//app.use("/", routes);
+//app.use("/user", usersRoute);
+//app.use("/dojos", dojosRoute);
+//app.use("/events", eventsRoute);
+//app.use("/badges", badgesRoute);
 
 let port = process.env.PORT || 3000;
 

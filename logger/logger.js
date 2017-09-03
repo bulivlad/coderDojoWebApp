65f7d4fcfdbd50d2d8aef77b6e4eb413224b7720
Catalin <catapara@gmail.com>
2017-09-03 11:19:32 +0300

    Creating logFiles directory for logs

--- logger/logger.js
+++ logger/logger.js
@@ -1,10 +1,8 @@
-const timestamp = () => (new Date()).toLocaleString();
-//const formatter = (options) => options.timestamp() +' '+ options.level.toUpperCase() +' '+ (options.message ? options.message : '') +
-//    (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : '' );;
-
+const fs = require('fs');
 const winston = require('winston');
 require('winston-daily-rotate-file');
 
+const timestamp = () => (new Date()).toLocaleString();
 
 winston.remove(winston.transports.Console);
 winston.add(winston.transports.Console, {
@@ -12,6 +10,14 @@ winston.add(winston.transports.Console, {
     //formatter: formatter,
     colorize: true,
 });
+
+//Creating the logFiles directory if it does not exist
+
+var dir = './logger/logFiles';
+
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
 
 let monthlyTransport = {
         filename: './logger/logFiles/log',

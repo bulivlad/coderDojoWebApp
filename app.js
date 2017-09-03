eacfeefc08d2bdd62eb072a52c0e9aeabcd1a5dc
Catalin <catapara@gmail.com>
2017-09-03 11:47:15 +0300

    Azure dev deploy trials.

--- app.js
+++ app.js
@@ -4,38 +4,38 @@
 
 const express = require("express"),
     path = require("path"),
-    //routes = require('./routes/index'),
-    //usersRoute = require('./routes/usersRoute'),
-    //dojosRoute = require('./routes/dojosRoute'),
-    //badgesRoute = require('./routes/badgesRoute'),
-    //eventsRoute = require('./routes/eventsRoute'),
-    //bodyParser = require('body-parser'),
-    //expressValidator = require("express-validator"),
-    //mongoose = require('mongoose'),
-    //expressSession = require('express-session'),
-    //cookieParser = require('cookie-parser'),
-    //passport = require('passport'),
-//passportLocal =   require('passport-local'),
+    routes = require('./routes/index'),
+    usersRoute = require('./routes/usersRoute'),
+    dojosRoute = require('./routes/dojosRoute'),
+    badgesRoute = require('./routes/badgesRoute'),
+    eventsRoute = require('./routes/eventsRoute'),
+    bodyParser = require('body-parser'),
+    expressValidator = require("express-validator"),
+    mongoose = require('mongoose'),
+    expressSession = require('express-session'),
+    cookieParser = require('cookie-parser'),
+    passport = require('passport'),
+    passportLocal =   require('passport-local'),
     logger = require('./logger/logger'),
-    //validator = require('./validator/validator'),
-    //helper = require('./auxiliary/helper'),
+    validator = require('./validator/validator'),
+    helper = require('./auxiliary/helper'),
     morgan = require('morgan');
-    //MongoStore = require('connect-mongo')(expressSession),
-    //https = require('https'),
+    MongoStore = require('connect-mongo')(expressSession),
+    https = require('https'),
     fs = require('fs');
 
 
 
-//let dataBaseName = process.env.MONGO_URI || 'mongodb://localhost/coderDojoTimisoara';
-////Connecting to the database
-//mongoose.connect(dataBaseName, function(err){
-//    if (err){
-//        console.log("Error:", err);
-//        logger.error('Cannot connect to database: ' + err);
-//    } else {
-//        logger.info('Connected to dbs: ' + dataBaseName);
-//    }
-//});
+let dataBaseName = process.env.MONGO_URI || 'mongodb://localhost/coderDojoTimisoara';
+//Connecting to the database
+mongoose.connect(dataBaseName, function(err){
+    if (err){
+        console.log("Error:", err);
+        logger.error('Cannot connect to database: ' + err);
+    } else {
+        logger.info('Connected to dbs: ' + dataBaseName);
+    }
+});
 
 //helper.initializeApp();
 
@@ -47,35 +47,35 @@ if(process.env.NODE_ENV){
 }
 
 ////Body parser middleware
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended: false}));
-//app.use(cookieParser());
-////TODO store secret as an environmental variable
-//app.use(expressSession({
-//    secret:'hj9g9897532u8904fsuig34534gggd',
-//    resave: false,
-//    saveUninitialized: false,
-//    store: new MongoStore({mongooseConnection: mongoose.connection})
-//}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+//TODO store secret as an environmental variable
+app.use(expressSession({
+    secret:'hj9g9897532u8904fsuig34534gggd',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
 
 //Setting the static directory where the client files(css, js) are kept
 app.use(express.static(path.join(__dirname, 'client')));
-//app.use(passport.initialize());//Grabs the data from the session
-//app.use(passport.session());//Puts data into local session
+app.use(passport.initialize());//Grabs the data from the session
+app.use(passport.session());//Puts data into local session
 
 // This middleware is used for validating fields, the error formatter is used to format the error message which will be
 // send to the client.
-//app.use(expressValidator({
-//    errorFormatter: validator.errorFormatter,
-//    customValidators: validator.customValidators
-//}));
+app.use(expressValidator({
+    errorFormatter: validator.errorFormatter,
+    customValidators: validator.customValidators
+}));
 
 //// We set the routes the data will take
-//app.use("/", routes);
-//app.use("/user", usersRoute);
-//app.use("/dojos", dojosRoute);
-//app.use("/events", eventsRoute);
-//app.use("/badges", badgesRoute);
+app.use("/", routes);
+app.use("/user", usersRoute);
+app.use("/dojos", dojosRoute);
+app.use("/events", eventsRoute);
+app.use("/badges", badgesRoute);
 
 let port = process.env.PORT || 3000;
 
